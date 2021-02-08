d51562138f452d3ecb5a2401441581e7db0385e7
dougmantis <dougmantis@gmail.com>
2021-02-08 12:03:25 -0700

    testing php stuff
    Signed-off-by: dougmantis <dougmantis@gmail.com>

---
+++ nav.php
@@ -0,0 +1,21 @@
+	<div class="top-ribbon">
+		<div class="logo">
+			<a  href="home.html"><img src="logodark.png" alt="BoblintheGoblin.com"></a>
+		</div>
+		<div class="nav menu">
+			<ul>
+				<li class = "page-button">
+					<a href="QuickCreator.html">Quick NPC Generator</a>
+				</li>
+				<li class = "page-button">
+					<a href="StandardCreator.html">Standard NPC Generator</a>
+				</li>
+				<li class = "page-button">
+					<a href="Load.html">Load NPC</a>
+				</li>
+				<li class = "page-button">
+					<a href="Login.html">Login</a>
+				</li>
+			</ul>
+		</div>
+	</div> 
